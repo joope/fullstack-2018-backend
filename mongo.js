2d045d9dbdea1f78817eba9a35cc0e862e3b018c
joope <pyryjoona@gmail.com>
2018-02-05 18:21:28 +0200

    3.12

---
+++ mongo.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const url = process.env.DB_URL;
+
+mongoose.connect(url);
+
+const Person = mongoose.model('Person', {
+  name: String,
+  number: String,
+  updatedAt: Date
+});
+
+const [a, b, name, number] = process.argv;
+
+if (name && number) {
+  const newPerson = new Person({
+    name,
+    number,
+    updatedAt: new Date()
+  });
+  newPerson
+    .save()
+    .then(response => {
+      console.log(`lisätään henkilö ${name} numero ${number} luetteloon`)
+      mongoose.connection.close();
+    })
+} else {
+  Person
+    .find({})
+    .then(res => {
+      console.log(`puhelinluettelo:`)
+      res.forEach(p => console.log(p.name, p.number));
+      mongoose.connection.close();
+    })
+}
